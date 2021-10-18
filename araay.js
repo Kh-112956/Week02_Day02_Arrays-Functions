2954d6fe85da0d89f42f2bf160dd4c79041af407
KHOLOD ALRWULIY <92294786+Kh-112956@users.noreply.github.com>
2021-10-18 22:31:32 +0300

    Add files via upload

---
+++ araay.js
@@ -0,0 +1,130 @@
+//Q(1)
+
+let numbers = [4, 2, 5, 6, 8, 9, 3];
+function mulitiood(num) {
+
+
+
+    for (let i = 0; i < num.length; i++) {
+
+        if (num[i] % 2 != 0) {
+
+            num[i] = num[i] * 2
+        }
+
+    } 
+    return num;
+}
+
+console.log(mulitiood(numbers));
+
+//=======================================================================
+//Q(2)
+let array = [4, 2, 5, 6, 8, 9, 3];
+function mulitieven(array) {
+
+
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i] % 2 == 0) {
+            array[i] = array[i] * 2;
+        }
+       
+    }
+    return array;
+
+}
+console.log(mulitieven(array));
+
+   
+    //=======================================================================
+    //Q(3)
+
+
+    let color1 = ["orange", "red", "biue"];
+    function check (color1) {
+
+    
+        
+
+    
+    console.log(color1);
+    for (let i = 0; i < color1.length; i++) {
+     
+    
+    
+        return color1;
+    
+        console.log("my #" + (i + 1) + " choice is " + color1[i]);
+}
+
+    }
+    //=======================================================================
+    //Q(4)
+
+    let kh = [2, 3, 4];{
+    function add(kh) {
+
+
+        console.log(kh);{
+        kh.unshift(0);
+        kh.unshift(1);
+        kh.push(6);
+        kh.push(5);
+        kh.push(7);
+    
+
+return kh;
+}
+        console.log(kh);
+}
+}
+    
+    //=====================================================================
+    //Q(5)
+    let arrys = [8, 12, 15, 1, 5, 20, 2];{
+    let h = 2;{
+    let ar = "";
+
+}
+
+for (let i = 0; i < arrys.length; i++) {
+    if (h == arrys[i]) {
+
+        ar = "true";
+        break;
+    }
+    else { t = "zero"; }
+}
+return arrys;
+}
+console.log(ar);
+
+
+//=======================================================================
+//  Q(6)
+
+let number = [5, 2, 7, 1, 9, 8,];
+
+console.log(number);
+
+let nm = 5;
+function check() {
+
+
+
+    for (let i = 0; i < number.length; i++) {
+
+        for (let j = i + 1; j < number.length - 1; j++) {
+
+            if (nm == (number[i] + number[j])) {
+
+                {
+                    return number; 
+                    console.log(number[i], number[j])
+
+                
+        
+        
+
